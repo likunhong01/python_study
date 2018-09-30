e72ee3986d8cf6d45a9b16305ab48c4874856eda
likunkun <42308753+likunhong01@users.noreply.github.com>
2018-09-30 11:39:37 +0800

    进程锁

---
+++ 10multiprocess/进程锁.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+#Version:python3.6.0
+#Tools:Pycharm 2017.3.2
+# Author:LIKUNHONG
+__date__ = '2018/9/18 12:42'
+__author__ = 'likunkun'
+
+from multiprocessing import Process, Lock
+
+def f(l, i):
+    l.acquire()
+    try:
+        print('hello world', i)
+    finally:
+        l.release()
+
+if __name__ == '__main__':
+    lock = Lock()
+    for num in range(10):
+        Process(target=f, args=(lock, num)).start()
