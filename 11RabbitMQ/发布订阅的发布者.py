51c3877655e35e57accb5408b9c364f4ac8ad5bb
likunkun <42308753+likunhong01@users.noreply.github.com>
2018-10-18 19:49:00 +0800

    发布与订阅的两端

---
+++ 11RabbitMQ/发布订阅的发布者.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+#Version:python3.6.0
+#Tools:Pycharm 2017.3.2
+# Author:LIKUNHONG
+__date__ = '2018/10/17 15:10'
+__author__ = 'likunkun'
+
+from 发布订阅 import RedisHelper
+obj = RedisHelper()
+obj.public('hello')
